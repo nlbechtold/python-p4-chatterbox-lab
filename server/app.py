1ac2df98e5418deea45f3a0eb50748a4d59dcdab
Nancy Bechtold <nancyleebechtold@gmail.com>
2024-08-07 18:08:26 -0600

    yaaaay

--- server/app.py
+++ server/app.py
@@ -14,13 +14,62 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=["GET","POST"])
 def messages():
-    return ''
+    print(request.method)
+    if request.method=="GET":
+        all_ms = Message.query.order_by(Message.created_at.asc()).all()
+        r_list = []
+        for message in all_ms:
+            r_list.append(message.to_dict())
+        return r_list,200
+    elif request.method=="POST":
 
-@app.route('/messages/<int:id>')
+        try:
+            data = request.get_json()
+            new_message = Message(
+                body = data["body"],
+                username=data["username"]
+            )
+            db.session.add(new_message)
+            db.session.commit()
+            return new_message.to_dict(),201
+        except Exception as e:
+            print(e)
+            return {
+                "Error": "Please input all values"
+            },400
+
+@app.route('/messages/<int:id>', methods=["GET","PATCH","DELETE"])
 def messages_by_id(id):
-    return ''
-
+  
+    message = Message.query.filter(Message.id == id).first()
+    if message:
+        if request.method == "GET":
+                return message.to_dict()
+        elif request.method=="PATCH":
+            try:
+                data = request.get_json()
+                for key in data:
+                    # setattr(object you are changing, key you are changing, what you are changing it to)
+                    setattr(message,key,data[key])
+                db.session.add(message)
+                db.session.commit()
+                return message.to_dict(),200
+            except Exception as e:
+                print(e)
+                return {
+                    "error": "Validation Error"
+                },400
+        elif request.method=="DELETE":
+            db.session.delete(message)
+            db.session.commit()
+            return {}
+                
+    else:
+        return {
+            "Error": "Not valid id"
+        },400
+    
 if __name__ == '__main__':
     app.run(port=5555)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, func
 from sqlalchemy_serializer import SerializerMixin
+# from app import app
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -12,3 +13,11 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=func.now())
+    updated_at = db.Column(db.DateTime, onupdate=func.now())
+
+
+def __repr__(self):
+    return f'<Message {self.id}, {self.body}>'
